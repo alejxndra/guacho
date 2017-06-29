6952b6b7f2cb789e82c8112fac39100011ace887
Liz Marin <lizmarin@Lizs-MacBook-Air.local>
2017-06-29 14:59:46 -0400

    inicio

--- routes/reservaciones.js
+++ routes/reservaciones.js
@@ -7,7 +7,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
